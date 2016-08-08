924d694c51569b6bbd9ecbc99fe767034fc82587
WaDoFa <163yixin@sina.com>
2016-08-08 14:51:42 +0800

    测试二

---
+++ javaScipt/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  javaScipt
+//
+//  Created by Macintosh HD on 16/8/8.
+//  Copyright © 2016年 Wang.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ javaScipt/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  javaScipt
+//
+//  Created by Macintosh HD on 16/8/8.
+//  Copyright © 2016年 Wang.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
